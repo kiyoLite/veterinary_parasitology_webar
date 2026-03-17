dbcd836a3e66aaa16570ebb8cfc06692c888585d
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-16 19:45:28 -0500

    create component for taxonomic-map with ng generate

---
+++ frontend/src/app/pages/taxonomic-map/taxonomic-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taxonomic-map',
+  imports: [],
+  templateUrl: './taxonomic-map.html',
+  styleUrl: './taxonomic-map.css',
+})
+export class TaxonomicMap {
+
+}
