0bb3b8ea70fb3142763a4fddc0fdf778b988767a
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-16 20:27:52 -0500

    add /toxonomic-map route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaxonomicMapComponent } from './pages/taxonomic-map/taxonomic-map';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'toxonomic-map',
+    component: TaxonomicMapComponent
+}];
