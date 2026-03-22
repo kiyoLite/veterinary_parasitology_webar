1ba51515ab83bd77e30d98ea7287cfc159810e36
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 10:56:42 -0500

    create component for nodes of taxonomic map

---
+++ frontend/src/app/components/taxonomic/taxonomic-node/taxonomic-node.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taxonomic-node',
+  imports: [],
+  templateUrl: './taxonomic-node.html',
+  styleUrl: './taxonomic-node.css',
+})
+export class TaxonomicNode {
+
+}
