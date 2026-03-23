ca2e0c22df613ec0596ccd6474194a60e127d120
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 23:10:49 -0500

    create popover component to display the parasites by deparment

---
+++ frontend/src/app/components/epidemiological/parasite-popover/parasite-popover.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parasite-popover',
+  imports: [],
+  templateUrl: './parasite-popover.html',
+  styleUrl: './parasite-popover.css',
+})
+export class ParasitePopover {
+
+}
