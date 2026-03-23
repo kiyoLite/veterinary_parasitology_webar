fd6ad2f0d910424acdeec47296f65ccff9adc1fd
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 22:55:23 -0500

    add route for general epidemiological mappage]

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { TaxonomicMapPageComponent } from './pages/taxonomic/taxonomic-map-page';
+import { GeneralEpidemiologicalMapPage } from './pages/general-epidemiological-map/general-epidemiological-map-page';
 
 export const routes: Routes = [{
     path: 'toxonomic-map',
     component: TaxonomicMapPageComponent
-}];
+},
+{
+    path: 'epidemiological-map',
+    component: GeneralEpidemiologicalMapPage
+}
+];
