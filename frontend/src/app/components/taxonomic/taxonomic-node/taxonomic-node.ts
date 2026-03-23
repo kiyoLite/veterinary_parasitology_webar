9278b2fa664e809cdb83b4c7cc8be0888c1942fd
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 20:51:05 -0500

    add output signal and method to communicate with others components to find out which subbranch has been selected

--- frontend/src/app/components/taxonomic/taxonomic-node/taxonomic-node.ts
+++ frontend/src/app/components/taxonomic/taxonomic-node/taxonomic-node.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, HostBinding, inject, input, output } from '@angular/core';
 import { TaxonomicCategory } from '../../../Interface/taxonomic-category.interface';
 import { RouterLink } from '@angular/router';
 import { TaxonomicRepository } from '../../../repository/taxonomic.repository';
@@ -14,11 +14,26 @@ export class TaxonomicNode {
   taxonomicData = input.required<TaxonomicCategory>();
   gridRow = input.required<number>();
   gridColumn = input.required<number>();
+  taxonomicNodeChild = output<{ row: number, column: number, id: number }>();
+
 
   getSubCatories(id: number): { name: string, id: number }[] {
     let subCategoriesRaw = this.taxonomicRepository.getSubNodes(id);
     let subCatories = subCategoriesRaw.map((current) => ({ name: current.name, id: current.id }));
     return subCatories;
   }
+
+  selectChildNode(childIDRaw: string) {
+    this.taxonomicNodeChild.emit(
+      {
+        row: this.gridRow() + 1,
+        column: this.gridColumn(),
+        id: parseInt(childIDRaw)
+      });
+  }
+
+
+
+
 
 }
