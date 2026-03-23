c13a2881a0cf10f7000f9b9bcae1baa196ea64ac
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 22:45:13 -0500

    create component for epidemilogical map

---
+++ frontend/src/app/components/epidemiological/epidemiological-map/epidemiological-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'epidemiological-map',
+  imports: [],
+  templateUrl: './epidemiological-map.html',
+  styleUrl: './epidemiological-map.css',
+})
+export class EpidemiologicalMap {
+
+}
