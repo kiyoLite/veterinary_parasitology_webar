55cd1916d1ccec76276c38697a0214482d71406b
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-22 22:41:48 -0500

    create page component for general epidemilogical map

---
+++ frontend/src/app/pages/general-epidemiological-map/general-epidemiological-map-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-epidemiological-map-page',
+  imports: [],
+  templateUrl: './general-epidemiological-map-page.html',
+  styleUrl: './general-epidemiological-map-page.css',
+})
+export class GeneralEpidemiologicalMapPage {
+
+}
