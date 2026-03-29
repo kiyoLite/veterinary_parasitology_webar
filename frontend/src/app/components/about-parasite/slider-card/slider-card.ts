4b4093133f61d2cd9bf711c8be1552d343b62680
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-29 13:13:26 -0500

    create slider card component

---
+++ frontend/src/app/components/about-parasite/slider-card/slider-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-card',
+  imports: [],
+  templateUrl: './slider-card.html',
+  styleUrl: './slider-card.css',
+})
+export class SliderCard {
+
+}
