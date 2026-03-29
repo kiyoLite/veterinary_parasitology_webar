5e34fa3ac2a47750f40653ffa75216bd70af45d1
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-29 18:58:18 -0500

    add required input for slider and add methods to navigate around slider items

--- frontend/src/app/components/about-parasite/slider/slider.ts
+++ frontend/src/app/components/about-parasite/slider/slider.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { ParasiteLifeCycle } from '../../../Interface/parasite-life-cycle.interface';
+import { SliderCard } from '../slider-card/slider-card';
 
 @Component({
   selector: 'slider',
-  imports: [],
+  imports: [SliderCard],
   templateUrl: './slider.html',
   styleUrl: './slider.css',
 })
 export class Slider {
+  items = input.required<ParasiteLifeCycle>();
+  selectedCardIndex = signal<number>(0);
+  setSelectCard(index: number) {
+    this.selectedCardIndex.set(index);
+  }
 
+  previousCard() {
+    this.selectedCardIndex.update(current => current > 0 ? current - 1 : current);
+  }
+
+  nextCard() {
+    const amountCards = this.items().length;
+    this.selectedCardIndex.update(current => (current - 1) < amountCards ? current + 1 : current);
+
+  }
 }
