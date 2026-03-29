2fd93f0da7ed9521af21d1f64a63e35997ffa030
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-29 12:33:31 -0500

    create two types for endpoint call )

---
+++ frontend/src/app/Interface/deparment-parasites.interface.ts
@@ -0,0 +1,9 @@
+export type ColombiaDepartment = 'Amazonas' | 'Antioquia' | 'Arauca' | 'Atlantico' | 'Bolivar' | 'Boyaca' |
+    'Cauca' | 'Cesar' | 'Choco' | 'Caldas' | 'Cordoba' | 'Caqueta' | 'Casanare' | 'Cundinamarca' |
+    'Distrito Capital de Bogota' | 'Guainia' | 'Guaviare' | 'Huila' | 'La Guajira' | 'Magdalena' |
+    'Meta' | 'Nariño' | 'Norte de Santander' | 'Putumayo' | 'Quindio' | 'Risaralda' |
+    'San Andres, Providencia y Santa Catalina' | 'Santander' | 'Sucre' | 'Tolima' | 'Valle del Cauca' |
+    'Vichada' | 'Vaupes'
+
+
+export type parasitesEachDeparment = Map<ColombiaDepartment, string[]>;
