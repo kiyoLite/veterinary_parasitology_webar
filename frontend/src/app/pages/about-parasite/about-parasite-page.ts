b20caf6162427631ffefe8c2262e9cf47b31c1dd
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-03-29 23:14:30 -0500

    intialize page component to show details about parasite

---
+++ frontend/src/app/pages/about-parasite/about-parasite-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Slider } from '../../components/about-parasite/slider/slider';
+
+@Component({
+  selector: 'app-about-parasite-page',
+  imports: [Slider],
+  templateUrl: './about-parasite-page.html',
+  styleUrl: './about-parasite-page.css',
+})
+export class AboutParasitePage {
+
+}
