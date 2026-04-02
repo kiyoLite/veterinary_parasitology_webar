ffd9df3d0fbf0f847f9d12bbf62a98345fb75ee5
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-02 17:19:23 -0500

    add route for about parasite page, include dynamic path for loading component data according to parasite

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TaxonomicMapPageComponent } from './pages/taxonomic/taxonomic-map-page';
 import { GeneralEpidemiologicalMapPage } from './pages/general-epidemiological-map/general-epidemiological-map-page';
+import { TemporalPage } from './pages/temporal-page/temporal-page';
+import { AboutParasitePage } from './pages/about-parasite/about-parasite-page';
 
 export const routes: Routes = [{
     path: 'toxonomic-map',
@@ -9,5 +11,10 @@ export const routes: Routes = [{
 {
     path: 'epidemiological-map',
     component: GeneralEpidemiologicalMapPage
+},
+{
+    path: 'about-parasite/:parasite',
+    component: AboutParasitePage
+
 }
 ];
