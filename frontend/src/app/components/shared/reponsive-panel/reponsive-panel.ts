1fdf03cbfe59f9460dc778b2c4f641d9cf43d2f8
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-02 20:07:02 -0500

    initilize a panel component (reusable component) for display  more details information about something

---
+++ frontend/src/app/components/shared/reponsive-panel/reponsive-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reponsive-panel',
+  imports: [],
+  templateUrl: './reponsive-panel.html',
+  styleUrl: './reponsive-panel.css',
+})
+export class ReponsivePanel {
+
+}
