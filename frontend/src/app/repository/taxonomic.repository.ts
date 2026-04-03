9220b47360fe02dc77a1d99e404382fbb57b2fe4
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-03 15:44:47 -0500

    remove unnecessary methods using in taxonomic map , map is now building with tree cdk so logic change, instead load tree content by piece ,  tree content will be load in once all at once

--- frontend/src/app/repository/taxonomic.repository.ts
+++ frontend/src/app/repository/taxonomic.repository.ts
@@ -1,51 +1,11 @@
 import { Injectable } from "@angular/core";
 import { TaxonomicCategory } from "../Interface/taxonomic-category.interface";
+import { parasitesEachDeparment } from "../Interface/deparment-parasites.interface";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TaxonomicRepository {
-    getRootNodes = function (): TaxonomicCategory[] {
-        return [{
-            category: "familia",
-            name: "parasito cool",
-            id: 2,
-            parasiteID: 3
-        },
-        {
-            category: "familia",
-            name: "parasito cool 2 ",
-            id: 3,
-        },
-        {
-            category: "familia",
-            name: "parasito cool 3",
-            id: 4,
-        }];
-    }
 
-    getSubNodes = function (nodeID: number): TaxonomicCategory[] {
-        return [
-            {
-                category: "subfamilia",
-                name: "subparasito cool2",
-                id: 3,
-            },
-            {
-                category: "subfamilia",
-                name: "subparasito cool3",
-                id: 4,
-                parasiteID: 1
-            }
-        ];
-    }
-
-    getNodeByID = function (nodeID: number): TaxonomicCategory | null {
-        return {
-            category: "subfamilia",
-            name: "subparasito cool3",
-            id: 3,
-        };
-    }
 
 }
