ec3cb2d8c7b58b3a8511843f25bfaf1009608d2a
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-03 15:49:47 -0500

    add method to be using for generate 1.the taxonomic-map 2.details side bar 3.location parasite (missing implementation all methods)

--- frontend/src/app/repository/taxonomic.repository.ts
+++ frontend/src/app/repository/taxonomic.repository.ts
@@ -7,5 +7,14 @@ import { parasitesEachDeparment } from "../Interface/deparment-parasites.interfa
 })
 export class TaxonomicRepository {
 
+    getMapData(): TaxonomicCategory[] {
+        throw new Error('Method not implemented.');
+    }
+    getDetailsFromTaxonomicCategory(category: string): string {
+        throw new Error('Method not implemented.');
+    }
+    getParasitesLocationByDeparment(): parasitesEachDeparment {
+        throw new Error('Method not implemented.');
+    }
 
 }
