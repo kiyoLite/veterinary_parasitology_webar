49b82c47dec6f0d2ef1848382bf5c428d178eb5e
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-02 21:01:32 -0500

    intialize component to display description about select taxonomic catogory (family,subfamily,gender...)

---
+++ frontend/src/app/components/taxonomic/taxonomic-category-details/taxonomic-category-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taxonomic-category-details',
+  imports: [],
+  templateUrl: './taxonomic-category-details.html',
+  styleUrl: './taxonomic-category-details.css',
+})
+export class TaxonomicCategoryDetails {
+
+}
