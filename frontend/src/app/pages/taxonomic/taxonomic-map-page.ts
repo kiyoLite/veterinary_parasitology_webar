ddea469e6f1bab313061eaa626ab63b2376edc71
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-02 23:50:09 -0500

    add require signals to display side panel content

--- frontend/src/app/pages/taxonomic/taxonomic-map-page.ts
+++ frontend/src/app/pages/taxonomic/taxonomic-map-page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { HeaderComponent } from "../../components/shared/header/header";
 import { TaxonomicMapComponent } from "../../components/taxonomic/taxonomic-map/taxonomic-map";
+import { PresenceParasites } from '../../components/shared/presence-parasites/presence-parasites';
+import { TaxonomicCategoryDetails } from '../../components/taxonomic/taxonomic-category-details/taxonomic-category-details';
+
+enum PickSidePanel {
+  PRESENCE_PARASITES, TAXONOMIC_CATEGORY_DETAILS, NONE
+}
+
 @Component({
   selector: 'taxonomic-map-page',
-  imports: [HeaderComponent, TaxonomicMapComponent],
+  imports: [HeaderComponent, TaxonomicMapComponent, PresenceParasites, TaxonomicCategoryDetails],
   templateUrl: './taxonomic-map-page.html',
   styleUrl: './taxonomic-map-page.css',
 })
 export class TaxonomicMapPageComponent {
+  pickSidePanelEnum = PickSidePanel;
+  pickSidePanel = signal<PickSidePanel>(this.pickSidePanelEnum.NONE);
+  pickTaxonomicCategory = signal<string | null>(null);
 
 }
