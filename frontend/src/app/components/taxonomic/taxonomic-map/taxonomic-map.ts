9e38325ce21a4cbe4c9239a238c91cf03715b4df
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-03 16:02:09 -0500

    remove all taxonomic map and use angular cdk -> tree cdk to build  map, the main reason is taxonomic map is display as tree , build custom logic with grid css to represent the tree, previous logic only allow display display single subnode from list subnodes (technical difficulties), wl taxonomic map and use angular cdk -> tree cdk to build  map, the main reason is taxonomic map isith angular cdk all logic is build only need provide require data , with cdk can display allly allow display display single subnode from list subnodes (technical difficulties), with angular  subnodes (one of the biggest complaint customer had about the previoius mad)

--- frontend/src/app/components/taxonomic/taxonomic-map/taxonomic-map.ts
+++ frontend/src/app/components/taxonomic/taxonomic-map/taxonomic-map.ts
@@ -1,7 +1,8 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TaxonomicRepository } from '../../../repository/taxonomic.repository';
 import { TaxonomicCategory } from '../../../Interface/taxonomic-category.interface';
-import { TaxonomicNode } from '../taxonomic-node/taxonomic-node';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { ArrayDataSource } from '@angular/cdk/collections';
 
 interface TaxonomicGridNode {
   category: TaxonomicCategory,
@@ -11,36 +12,16 @@ interface TaxonomicGridNode {
 
 @Component({
   selector: 'taxonomic-map',
-  imports: [TaxonomicNode],
+  imports: [CdkTreeModule],
   templateUrl: './taxonomic-map.html',
   styleUrl: './taxonomic-map.css',
 })
 export class TaxonomicMapComponent {
-  taxonomicRepository = inject(TaxonomicRepository);
-  rootNodes = this.taxonomicRepository.getRootNodes();
-  taxonomicRootNodes: TaxonomicGridNode[] = this.rootNodes.map((current, index) => ({
-    category: current,
-    gridColumn: index + 1,
-    gridRow: 1
-  }))
-  nodes = signal<TaxonomicGridNode[]>(this.taxonomicRootNodes);
-  mapGridColumns = this.taxonomicRootNodes.length;
-  mapGridRows = signal(1);
-
-
-
-  getStyleGridColumns() {
-    return 'repeat(' + this.mapGridColumns + ' , 1fr)';
-  }
-
-  styleGridRows = computed(() => 'repeat(' + this.mapGridRows() + ', 1fr)');
-
-  addNode(node: { column: number, row: number; id: number; }) {
-    const newNode = this.taxonomicRepository.getNodeByID(node.id);
-    if (newNode == null) return;
-    const newGridNode: TaxonomicGridNode = { category: newNode, gridColumn: node.column, gridRow: node.row };
-    this.mapGridRows.update(current => node.row > current ? node.row : current);
-    this.nodes.update(list => [...list, newGridNode]);
-  }
-
+  private taxonomicRepository = inject(TaxonomicRepository);
+  private mapNodesData: TaxonomicCategory[] = this.taxonomicRepository.getMapData();
+  parasites = new ArrayDataSource<TaxonomicCategory>(this.mapNodesData);
+  childrenAccessor = (node: TaxonomicCategory) => node.children ?? [];
+  hasChildren = (_: number, node: TaxonomicCategory) => !!node.children && node.children.length > 0;
 }
+
+
