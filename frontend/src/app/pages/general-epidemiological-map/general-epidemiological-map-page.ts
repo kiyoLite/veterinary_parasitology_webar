c89569dd583f9fa2bb8dcc8141954550def57f74
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-03 18:36:44 -0500

    add missing template's dependency

--- frontend/src/app/pages/general-epidemiological-map/general-epidemiological-map-page.ts
+++ frontend/src/app/pages/general-epidemiological-map/general-epidemiological-map-page.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { EpidemiologicalMap } from '../../components/epidemiological/epidemiological-map/epidemiological-map';
 import { EpidemiologicalRepostiory } from '../../repository/epidemological.repository';
+import { HeaderComponent } from "../../components/shared/header/header";
+import { PresenceParasites } from "../../components/shared/presence-parasites/presence-parasites";
 
 @Component({
   selector: 'app-general-epidemiological-map-page',
-  imports: [EpidemiologicalMap],
+  imports: [EpidemiologicalMap, HeaderComponent, PresenceParasites],
   templateUrl: './general-epidemiological-map-page.html',
   styleUrl: './general-epidemiological-map-page.css',
 })
