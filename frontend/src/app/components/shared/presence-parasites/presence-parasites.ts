dda85f70bdc2b343b76b9a394d4e738d39d85534
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-02 20:58:56 -0500

    initialize component to display data about presence parasite

---
+++ frontend/src/app/components/shared/presence-parasites/presence-parasites.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presence-parasites',
+  imports: [],
+  templateUrl: './presence-parasites.html',
+  styleUrl: './presence-parasites.css',
+})
+export class PresenceParasites {
+
+}
