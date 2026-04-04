a72afd81f6fd6b1ffa403f9c14df4db06b27395f
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-03 23:08:37 -0500

    intialize component to display parasite model (3D o AR)

---
+++ frontend/src/app/components/about-parasite/parasite-model/parasite-model.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parasite-model',
+  imports: [],
+  templateUrl: './parasite-model.html',
+  styleUrl: './parasite-model.css',
+})
+export class ParasiteModel {
+
+}
