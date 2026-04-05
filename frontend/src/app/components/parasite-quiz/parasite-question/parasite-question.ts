9fe11ea7fcf82d4a98a7ae557e70e44dfab1c09d
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-04 20:34:27 -0500

    initiliaze component to display a single question from parasite quiz

---
+++ frontend/src/app/components/parasite-quiz/parasite-question/parasite-question.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parasite-question',
+  imports: [],
+  templateUrl: './parasite-question.html',
+  styleUrl: './parasite-question.css',
+})
+export class ParasiteQuestion {
+
+}
