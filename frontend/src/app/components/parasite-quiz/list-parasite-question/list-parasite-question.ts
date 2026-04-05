9dfa19cb33efda09170b38bae63e2bceaf487753
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-04 20:38:00 -0500

    initialize component for displaying list of question from parasite quiz , porpouse of this component make quiz page component manage easier question validation , user input , outside page (redirection) . delegate display question to this component

---
+++ frontend/src/app/components/parasite-quiz/list-parasite-question/list-parasite-question.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-parasite-question',
+  imports: [],
+  templateUrl: './list-parasite-question.html',
+  styleUrl: './list-parasite-question.css',
+})
+export class ListParasiteQuestion {
+
+}
