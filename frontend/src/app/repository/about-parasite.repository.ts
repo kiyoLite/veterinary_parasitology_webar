2643be04e64096caea215d8946cd5ab18a790cf9
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-04 19:57:35 -0500

    (test commit) implements repository method to return mocking data

--- frontend/src/app/repository/about-parasite.repository.ts
+++ frontend/src/app/repository/about-parasite.repository.ts
@@ -7,10 +7,26 @@ import { ParasiteLifeCycle } from "../Interface/parasite-life-cycle.interface";
 export class AboutParasiteRepository {
 
     getParasiteLifeCycle(parasite: string): ParasiteLifeCycle {
-        throw new Error("not implemented yet")
+        return [
+            {
+                stage: "Infiltración Silenciosa",
+                imageUrl: "assets/icons/magnifying-glass.svg",
+                description: "El ciclo comienza con una pequeña espora que desciende desde la atmósfera. Al eclosionar, una criatura similar a un gusano busca perforar la piel del huésped para llegar al sistema nervioso central."
+            },
+            {
+                stage: "Maduración y Sustitución",
+                imageUrl: "assets/icons/magnifying-glass.svg",
+                description: "Una vez alcanzado el cerebro o un órgano vital, el parásito consume la estructura original y la reemplaza por completo, asumiendo el control de las funciones motoras y cognitivas del huésped."
+            },
+            {
+                stage: "Mimetismo Adaptativo",
+                imageUrl: "assets/icons/magnifying-glass.svg",
+                description: "En su etapa adulta, el parásito puede manipular su estructura celular a voluntad, permitiéndole endurecer tejidos para el combate o alterar su apariencia física para pasar desapercibido entre los humanos."
+            }
+        ];
     }
 
     getParasiteDetails(parasite: string): string {
-        throw new Error("not implemented yer")
+        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'.repeat(10)
     }
 }
