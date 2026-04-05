07c3a9df2e40505add586addf4e928934b8c5d31
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-04 20:22:39 -0500

    add parasite quiz page component to the path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TaxonomicMapPageComponent } from './pages/taxonomic/taxonomic-map-page';
 import { GeneralEpidemiologicalMapPage } from './pages/general-epidemiological-map/general-epidemiological-map-page';
 import { AboutParasitePage } from './pages/about-parasite/about-parasite-page';
+import { ParasiteQuiz } from './pages/parasite-quiz/parasite-quiz';
 
 export const routes: Routes = [{
     path: 'taxonomic-map',
@@ -16,8 +17,12 @@ export const routes: Routes = [{
     component: GeneralEpidemiologicalMapPage
 },
 {
+    path: 'parasite-quiz/:parasite',
+    component: ParasiteQuiz
+},
+{
     path: 'about-parasite/:parasite',
-    component: AboutParasitePage
-
+    component: AboutParasitePage,
 }
+
 ];
