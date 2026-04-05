b08b20bad953e0c54745435392716834053db864
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-04 20:22:00 -0500

    initialize page component to display quiz about pasarasite to assess knowledge acquired in about parasite page

---
+++ frontend/src/app/pages/parasite-quiz/parasite-quiz.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parasite-quiz',
+  imports: [],
+  templateUrl: './parasite-quiz.html',
+  styleUrl: './parasite-quiz.css',
+})
+export class ParasiteQuiz {
+
+}
