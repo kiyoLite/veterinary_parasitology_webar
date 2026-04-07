5e474c8b1d561179cc44a339f69981a4f885ffba
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-07 14:28:37 -0500

    component display list of question from parasite quiz , add require signal , template and basic style

--- frontend/src/app/components/parasite-quiz/list-parasite-question/list-parasite-question.ts
+++ frontend/src/app/components/parasite-quiz/list-parasite-question/list-parasite-question.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Quiz } from '../../../Interface/quiz.interface';
+import { ParasiteQuestion } from "../parasite-question/parasite-question";
 
 @Component({
   selector: 'app-list-parasite-question',
-  imports: [],
+  imports: [ParasiteQuestion],
   templateUrl: './list-parasite-question.html',
   styleUrl: './list-parasite-question.css',
 })
 export class ListParasiteQuestion {
-
+  questions = input.required<Quiz>();
+  isSubmit = input.required<boolean>();
 }
