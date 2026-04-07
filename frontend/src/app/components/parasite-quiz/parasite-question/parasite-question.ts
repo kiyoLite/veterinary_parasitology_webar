0fa5e7319fa2c48f29ccbd08e64ab56d185e8202
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-07 14:30:26 -0500

    component use in parasite-quiz page to represent a single question about parasite , add required signal , template and basic style

--- frontend/src/app/components/parasite-quiz/parasite-question/parasite-question.ts
+++ frontend/src/app/components/parasite-quiz/parasite-question/parasite-question.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { QuizQuestion } from '../../../Interface/quiz.interface';
 
 @Component({
   selector: 'app-parasite-question',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './parasite-question.css',
 })
 export class ParasiteQuestion {
+  question = input.required<QuizQuestion>();
+  isSubmit = input.required<boolean>();
+
+
+
+  private isSelectOption(optionsForm: HTMLFormElement, optionIndex: number) {
+    const formData = new FormData(optionsForm);
+    const selectOption = formData.get("question") as number | null;
+    return selectOption == optionIndex
+  }
+
+  isCorrectOption(optionsForm: HTMLFormElement, optionIndex: number) {
+    return this.isSelectOption(optionsForm, optionIndex) && optionIndex == this.question().correctAnswerIndex;
+  }
+
+  isIncorrectOption(optionsForm: HTMLFormElement, optionIndex: number) {
+    return this.isSelectOption(optionsForm, optionIndex) && optionIndex != this.question().correctAnswerIndex;
+  }
+
 
 }
