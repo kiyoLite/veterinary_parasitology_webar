53505d36407d251692d2c144bbb9fae4bd3a9360
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-08 04:23:26 -0500

    add router link to dependency for be using in template

--- frontend/src/app/pages/about-parasite/about-parasite-page.ts
+++ frontend/src/app/pages/about-parasite/about-parasite-page.ts
@@ -2,14 +2,14 @@ import { Component, computed, inject } from '@angular/core';
 import { Slider } from '../../components/about-parasite/slider/slider';
 import { HeaderComponent } from "../../components/shared/header/header";
 import { ParasiteModel } from "../../components/about-parasite/parasite-model/parasite-model";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { AboutParasiteRepository } from '../../repository/about-parasite.repository';
 
 @Component({
   selector: 'app-about-parasite-page',
-  imports: [Slider, HeaderComponent, ParasiteModel],
+  imports: [Slider, HeaderComponent, ParasiteModel, RouterLink],
   templateUrl: './about-parasite-page.html',
   styleUrl: './about-parasite-page.css',
 })
