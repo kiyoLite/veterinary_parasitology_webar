e26cc52a1fa0bd41552115c0e1701317115d505b
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-14 17:56:10 -0500

    initialize page component to display epidemiological map according to parasite

---
+++ frontend/src/app/pages/parasite-epidemiological-map/parasite-epidemiological-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parasite-epidemiological-map',
+  imports: [],
+  templateUrl: './parasite-epidemiological-map.html',
+  styleUrl: './parasite-epidemiological-map.css',
+})
+export class ParasiteEpidemiologicalMap {
+
+}
