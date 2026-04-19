78ab585ed7ac11ad8a22e0b85ad47cb434562171
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-19 13:16:38 -0500

    get parasite name from route

--- frontend/src/app/pages/parasite-epidemiological-map/parasite-epidemiological-map.ts
+++ frontend/src/app/pages/parasite-epidemiological-map/parasite-epidemiological-map.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EpidemiologicalMap } from "../../components/epidemiological/epidemiological-map/epidemiological-map";
+import { HeaderComponent } from "../../components/shared/header/header";
+import { PresenceParasites } from "../../components/shared/presence-parasites/presence-parasites";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-parasite-epidemiological-map',
-  imports: [],
+  imports: [EpidemiologicalMap, HeaderComponent, PresenceParasites],
   templateUrl: './parasite-epidemiological-map.html',
   styleUrl: './parasite-epidemiological-map.css',
 })
 export class ParasiteEpidemiologicalMap {
+  private activateRouter = inject(ActivatedRoute);
+  parasite = toSignal(this.activateRouter.params.pipe(map(params => params['parasite'])));
 
 }
