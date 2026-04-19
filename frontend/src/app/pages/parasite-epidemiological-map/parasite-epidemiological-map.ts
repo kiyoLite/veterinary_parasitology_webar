f17f7068e933fa9497e2b85ea949b847a973512e
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2026-04-19 13:51:00 -0500

    add property to get deparments where parasite exist

--- frontend/src/app/pages/parasite-epidemiological-map/parasite-epidemiological-map.ts
+++ frontend/src/app/pages/parasite-epidemiological-map/parasite-epidemiological-map.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { EpidemiologicalMap } from "../../components/epidemiological/epidemiological-map/epidemiological-map";
 import { HeaderComponent } from "../../components/shared/header/header";
 import { PresenceParasites } from "../../components/shared/presence-parasites/presence-parasites";
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { EpidemiologicalRepostiory } from '../../repository/epidemological.repository';
 
 @Component({
   selector: 'app-parasite-epidemiological-map',
@@ -14,6 +15,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
 })
 export class ParasiteEpidemiologicalMap {
   private activateRouter = inject(ActivatedRoute);
+  private repository = inject(EpidemiologicalRepostiory);
   parasite = toSignal(this.activateRouter.params.pipe(map(params => params['parasite'])));
+  parasiteLocations = computed(() => this.repository.getDeparmentsParasiteExist(this.parasite()));
 
 }
